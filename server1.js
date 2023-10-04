6643edcad2b0f14d5a7ca41132e7ef7f6a01c02f
Elina Ausmees <elina.ausmees@gmail.com>
2023-10-04 17:21:02 +0300

    week 1

---
+++ server1.js
@@ -0,0 +1,17 @@
+//console.log("hello!");
+
+let firstName = 'Mihkel';
+let lastName = 'Jaakson'
+
+//console.log(firstName);
+
+//console.log("hello, " + firstName + "!");
+
+//template literals
+
+//console.log (`hello, ${firstName} ${lastName}!`);
+
+
+let greeting = `hello, ${firstName} ${lastName}!!!`;
+
+console.log(greeting);
