6b4222dfca0c4c41eb3c624c2c5aae964839e395
h0n3stm4n <144939012+h0n3stm4n@users.noreply.github.com>
2023-10-09 13:18:35 +0300

    Add files via upload

---
+++ week2/server2.js
@@ -0,0 +1,29 @@
+const userName = "Harry";
+let friends = ['Ron', 'Hermione', 'Hedwig'];
+friends.push('Draco');
+
+console.log(`${userName} has got ${friends.length} friends.`);
+
+for(let i=0; i < friends.length; i++) {
+    if(friends[i] === 'Draco') {
+        notFriends(friends[i]);
+        continue;
+    }
+    friends[i] = friends[i] + '*';
+    greeting(friends[i]);
+}
+
+friends.forEach(friend => {
+    console.log(friend);
+});
+
+function greeting(friendsName) {
+    console.log(`Hello, ${friendsName}!`);
+}
+
+function notFriends(enemyName) {
+    console.log(`We are not friends, ${enemyName}. Bye!`);
+}
+
+
+
